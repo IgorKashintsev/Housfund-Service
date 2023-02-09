85206b8a5e3470254d32eb2d7b72309ec3942b6e
IgorKashintsev <igorchello99@yandex.ru>
2023-02-09 18:37:53 +0300

    ver 2.2 Redux Toolkit

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,9 +4,6 @@ import { modalReducer } from './modal/slice';
 import { loadingReducer } from './loading/slice';
 import { usersReducer } from './users/slice';
 
-// export const composeEnhancers =
-//   window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
 const rootReducer = combineReducers({
   dropdown: dropdownReducer,
   loading: loadingReducer,
@@ -15,23 +12,10 @@ const rootReducer = combineReducers({
 });
 
 export type StoreState = ReturnType<typeof rootReducer>;
-// export const store = createStore(rootReducer, composeEnhancers());
 export const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({
     serializableCheck: false,
   }),
-  // middleware: (getDefaultMiddleware) => getDefaultMiddleware({
-  //   serializableCheck: {
-  //     ignoredActions: [
-  //       FLUSH,
-  //       REHYDRATE,
-  //       PAUSE,
-  //       PERSIST,
-  //       PURGE,
-  //       REGISTER,
-  //     ],
-  //   },
-  // }),
   devTools: process.env.NODE_ENV !== 'production',
 });
